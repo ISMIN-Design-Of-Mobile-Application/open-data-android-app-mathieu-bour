fc8d8a181e7a552038e7c2600d3d1ab0f607e980
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-12-20 19:18:31 +0100

    feat: add search bar (#8)
    * feat: add search bar
    
    * fix(actions): allow branches with '/'

--- app/src/main/java/fr/mathieubour/fetedelascience/ui/main/ListFragment.kt
+++ app/src/main/java/fr/mathieubour/fetedelascience/ui/main/ListFragment.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,11 +18,15 @@ import fr.mathieubour.fetedelascience.EventDetailsActivity
 import fr.mathieubour.fetedelascience.R
 import fr.mathieubour.fetedelascience.data.Event
 import fr.mathieubour.fetedelascience.models.MainViewModel
+import java.util.*
 
 /**
  * Holds the event list.
  */
 class ListFragment : Fragment() {
+    private lateinit var model: MainViewModel
+    private lateinit var filter_toggle: ImageView
+    private lateinit var search: EditText
     private lateinit var adapter: EventListAdapter
     private lateinit var eventsListObserver: Observer<List<Event>>
 
@@ -38,11 +45,12 @@ class ListFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        setupSearch()
         setupRecyclerView()
 
-        val model: MainViewModel = activity?.run {
+        model = activity!!.run {
             ViewModelProviders.of(this)[MainViewModel::class.java]
-        } ?: throw Exception("Invalid Activity")
+        }
 
         // Setup event list observer
         eventsListObserver = Observer { newEvents -> adapter.replace(newEvents) }
@@ -65,5 +73,25 @@ class ListFragment : Fragment() {
 
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
+    }
+
+    private fun setupSearch() {
+        filter_toggle = activity!!.findViewById(R.id.filter_toggle)
+        search = activity!!.findViewById(R.id.search)
+
+        filter_toggle.setOnClickListener {
+            search.visibility = if (search.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        search.doAfterTextChanged {
+            val filteredList = model.eventsList.value?.filter { event ->
+                val query = search.text.toString().toLowerCase(Locale.getDefault())
+
+                event.name.toLowerCase(Locale.getDefault()).contains(query)
+                        || event.city.toLowerCase(Locale.getDefault()).contains(query)
+            }
+
+            filteredList?.let(adapter::replace)
+        }
     }
 }
